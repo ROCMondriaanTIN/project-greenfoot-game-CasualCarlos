e4a9576c4c7b6781f67c771ed311090362c006eb
Carlos Miguel <302452254@student.rocmondriaan.nl>
2018-11-13 09:42:06 +0100

    Hero has received a function to pick up coins and save the amount of coins picked up.

--- Hero.java
+++ Hero.java
@@ -10,6 +10,7 @@ public class Hero extends Mover {
     private final double gravity;
     private final double acc;
     private final double drag;
+    public int coin;
     public Hero() {
         super();
         gravity = 9.8;
@@ -23,14 +24,13 @@ public class Hero extends Mover {
     @Override
     public void act() {
         handleInput();
-        
+        addCoin();
         velocityX *= drag;
         velocityY += acc;
         if (velocityY > gravity) {
             velocityY = gravity;
         }
         applyVelocity();
-
         for (Actor enemy : getIntersectingObjects(Enemy.class)) {
             if (enemy != null) {
                 //getWorld().removeObject(this);
@@ -40,7 +40,13 @@ public class Hero extends Mover {
         }
             
     }
-
+    public int addCoin(){
+         if(isTouching(coinBronze.class)){
+            removeTouching(coinBronze.class);
+            coin ++;
+        }
+        return coin;
+    }
     public void handleInput() {
         if (Greenfoot.isKeyDown("space")) {
             velocityY = -10;
